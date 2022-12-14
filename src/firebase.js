ce033175b75f006957e971753af0c8ea6dbbdb48
shanesharareh <shane.s.sharareh@gmail.com>
2022-12-14 15:23:27 -0800

    Create header skeleton

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBet4JIjaXcOkCFE6t2FCe5HO-1_IlOeZU",
+  authDomain: "facebook-clone-887b6.firebaseapp.com",
+  projectId: "facebook-clone-887b6",
+  storageBucket: "facebook-clone-887b6.appspot.com",
+  messagingSenderId: "588997804677",
+  appId: "1:588997804677:web:65b1b403cf8e569df61fec",
+  measurementId: "G-FZ4EMK8V3B"
+};
