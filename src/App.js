593a75c1e2a2873ad9fa0b81ce3438496772bbb9
shanesharareh <shane.s.sharareh@gmail.com>
2022-12-15 20:02:40 -0800

    finish basic  header elements

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import Header from './Header';
 function App() {
   return (
     <div className="App">
-     <h1>Lets build the facebook clone!</h1>
-     
      <Header/>
-
+     
      {/* App body */}
 
      {/* Sidebar */}
