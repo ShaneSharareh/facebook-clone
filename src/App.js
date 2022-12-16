94e40fe8b08fbb79e6ce5844d96b2f689fdfa8c4
shanesharareh <shane.s.sharareh@gmail.com>
2022-12-16 14:37:38 -0800

    Create story reel for the feed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-import Sidebar from './Sidebar'
+import Sidebar from './Sidebar';
+import Feed from './Feed';
 function App() {
   return (
-    <div className="App">
+    <div className="app">
      <Header/>
      <div className="app__body">
       <Sidebar />
+     < Feed />
      {/* Sidebar */}
-     {/* Feed */}
      </div>
     </div>
   );
---
+++ src/StoryReel.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./StoryReel.css"
+import Story from './Story.js'
+function StoryReel() {
+  return (
+    <div className="storyReel">
+        <Story 
+            image="https://townsquare.media/site/366/files/2019/07/Iron_Maiden_Barclays_Center_2019-1.jpg?w=980&q=75"
+            profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9SXirbEEVIN3Z9EzD0dUuQB9wUbD5W6HEmA&usqp=CAU"
+            title="Bruce Dickenson"
+            />
+              <Story 
+            image="https://townsquare.media/site/366/files/2019/07/Iron_Maiden_Barclays_Center_2019-1.jpg?w=980&q=75"
+            profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9SXirbEEVIN3Z9EzD0dUuQB9wUbD5W6HEmA&usqp=CAU"
+            title="Bruce Dickenson"
+            />
+                  <Story 
+            image="https://townsquare.media/site/366/files/2019/07/Iron_Maiden_Barclays_Center_2019-1.jpg?w=980&q=75"
+            profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9SXirbEEVIN3Z9EzD0dUuQB9wUbD5W6HEmA&usqp=CAU"
+            title="Bruce Dickenson"
+            />
+                       <Story 
+            image="https://townsquare.media/site/366/files/2019/07/Iron_Maiden_Barclays_Center_2019-1.jpg?w=980&q=75"
+            profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9SXirbEEVIN3Z9EzD0dUuQB9wUbD5W6HEmA&usqp=CAU"
+            title="Bruce Dickenson"
+            />
+
+    </div>
+  )
+}
+
+export default StoryReel
