5ed0f6bed06020181c3d1f511d4514a8663ee932
shanesharareh <shane.s.sharareh@gmail.com>
2022-12-16 12:37:15 -0800

    Create sidebar with basic widgets

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar'
 function App() {
   return (
     <div className="App">
      <Header/>
-     
-     {/* App body */}
-
+     <div className="app__body">
+      <Sidebar />
      {/* Sidebar */}
-
      {/* Feed */}
+     </div>
     </div>
   );
 }
